0ab7fcdf58d43d0249aeb502794aff3f81b391fc
Suyog Bargule <fti-sbargule@fti-lap-34.futopstech.com>
2024-10-01 18:22:27 +0530

    Initial Next.js static site commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",  // Default to "" if not set
+  basePath: "",
   images: { unoptimized: true },
   reactStrictMode: true,
   swcMinify: true,
