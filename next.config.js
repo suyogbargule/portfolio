b9caa8ce3cc419b044204ab84c658015505c9e83
Suyog Bargule <fti-sbargule@fti-lap-34.futopstech.com>
2025-04-23 11:10:05 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {unoptimized: true},
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {unoptimized: true},
+  output: 'export', // Enables static export
+  images: {
+    unoptimized: true, // Required for static image export
+  },
+  basePath: process.env.BASE_PATH || '', // Useful if deploying under a subpath
+  assetPrefix: process.env.BASE_PATH || '', // Same as basePath
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
