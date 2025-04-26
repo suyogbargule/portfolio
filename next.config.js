4196a118d107462f92c093e004507d9c94fae68c
Suyog Bargule <fti-sbargule@fti-lap-34.futopstech.com>
2025-04-26 12:29:16 +0530

    old

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  output: 'export', // Enables static export for GitHub Pages
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  images: {unoptimized: true},
   reactStrictMode: true,
   swcMinify: true,
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
