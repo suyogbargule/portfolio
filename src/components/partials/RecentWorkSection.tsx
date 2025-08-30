b80d9f490b1e1429da9289c5744d0fffa6cee856
GETAAILPUBT01 <getaailpubt01@getaailpubt01-Notebook-PC.bbrouter>
2025-08-30 23:26:25 +0530

    add project show lenght change

--- src/components/partials/RecentWorkSection.tsx
+++ src/components/partials/RecentWorkSection.tsx
@@ -9,7 +9,7 @@ const RecentWorkSection = () => {
       <SectionTitle>Corporate Projects</SectionTitle>
       <div className="mt-10 grid gap-4 xs:grid-cols-2 md:grid-cols-3 md:gap-8">
         {works
-          .filter((_, index) => index < 6)
+          .filter((_, index) => index < 9)
           .map((work) => (
             <Portfolio
               key={work.id}
