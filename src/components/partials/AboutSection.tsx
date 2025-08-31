12962703752760ede24b8fba02f03ce88c44e9de
GETAAILPUBT01 <getaailpubt01@getaailpubt01-Notebook-PC.bbrouter>
2025-08-31 12:06:26 +0530

    issue solve

--- src/components/partials/AboutSection.tsx
+++ src/components/partials/AboutSection.tsx
@@ -11,21 +11,24 @@ const AboutSection = () => {
         <SectionTitle>About Me</SectionTitle>
         <div className="grid grid-cols-1 gap-6 py-6 md:grid-cols-2 lg:gap-8">
           {/* Bio */}
-          <div className="">
+          <div>
             <p className="text-justify">
               Hi, I’m Suyog Hanmant Bargule, an AI & Computer Vision Engineer from Pune, India.
               I specialize in designing, training, and deploying neural network models, with expertise in Generative AI, Video AI, IoT, streaming, and image processing.
               My work involves building scalable AI solutions and problem-solving workflows using generative models, applied to real-world use cases like intelligent surveillance systems, real-time video analytics, and interactive AI-powered tools.
               I’m passionate about combining cutting-edge AI research with practical engineering to create impactful solutions that solve real problems and enhance user experiences.
             </p>
-            <a href="https://suyogbargule.github.io/portfolio/suyog_resume.pdf" download="https://suyogbargule.github.io/portfolio/suyog_resume.pdf">
+            <a
+              href="https://suyogbargule.github.io/portfolio/suyog_resume.pdf"
+              download="suyog_resume.pdf"
+            >
               <Button className="mt-5">Download CV</Button>
             </a>
           </div>
 
           {/* Skills */}
           <div className="grid grid-cols-2 gap-x-6 gap-y-4">
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Generative AI</h6>
                 <p>90%</p>
@@ -33,7 +36,7 @@ const AboutSection = () => {
               <ProgressBar color="blue" progress={90} />
             </div>
 
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Natural Language Processing (NLP)</h6>
                 <p>85%</p>
@@ -41,7 +44,7 @@ const AboutSection = () => {
               <ProgressBar color="purple" progress={85} />
             </div>
 
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Image Processing / Computer Vision</h6>
                 <p>92%</p>
@@ -49,7 +52,7 @@ const AboutSection = () => {
               <ProgressBar color="amber" progress={92} />
             </div>
 
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Deep Learning</h6>
                 <p>88%</p>
@@ -57,7 +60,7 @@ const AboutSection = () => {
               <ProgressBar color="indigo" progress={88} />
             </div>
 
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Model Training</h6>
                 <p>86%</p>
@@ -65,7 +68,7 @@ const AboutSection = () => {
               <ProgressBar color="rose" progress={86} />
             </div>
 
-            <div className="">
+            <div>
               <div className="mb-3 flex justify-between">
                 <h6 className="font-semibold">Backend</h6>
                 <p>80%</p>
@@ -73,47 +76,49 @@ const AboutSection = () => {
               <ProgressBar color="emerald" progress={80} />
             </div>
           </div>
+        </div>
 
-          {/* Overview */}
-          <div className="mt-10 grid grid-cols-2 gap-4 md:grid-cols-4">
-            <div className="flex">
-              <div className="mr-5 text-gray-300">
-                <HiOutlineFire size={50} />
-              </div>
-              <div>
-                <h2 className="text-3xl font-bold">4+</h2>
-                <p className="mt-1">Projects Completed</p>
-              </div>
+        {/* Overview */}
+        <div className="mt-10 grid grid-cols-2 gap-4 md:grid-cols-4">
+          <div className="flex">
+            <div className="mr-5 text-gray-300">
+              <HiOutlineFire size={50} />
             </div>
-            <div className="flex">
-              <div className="mr-5 text-gray-300">
-                <FiCoffee size={50} />
-              </div>
-              <div>
-                <h2 className="text-3xl font-bold">2</h2>
-                <p className="mt-1">Cup of Coffee</p>
-              </div>
+            <div>
+              <h2 className="text-3xl font-bold">4+</h2>
+              <p className="mt-1">Projects Completed</p>
             </div>
+          </div>
 
-            <div className="flex">
-              <div className="mr-5 text-gray-300">
-                <HiOutlineUsers size={50} />
-              </div>
-              <div>
-                <h2 className="text-3xl font-bold">4+</h2>
-                <p className="mt-1">Satisfied clients</p>
-              </div>
+          <div className="flex">
+            <div className="mr-5 text-gray-300">
+              <FiCoffee size={50} />
             </div>
+            <div>
+              <h2 className="text-3xl font-bold">2</h2>
+              <p className="mt-1">Cup of Coffee</p>
+            </div>
+          </div>
 
-            <div className="flex">
-              <div className="mr-5 text-gray-300">
-                <HiOutlineChartBar size={50} />
-              </div>
-              <div>
-                <h2 className="text-3xl font-bold">3+</h2>
-                <p className="mt-1">Years of experience</p>
-              </div>
+          <div className="flex">
+            <div className="mr-5 text-gray-300">
+              <HiOutlineUsers size={50} />
             </div>
+            <div>
+              <h2 className="text-3xl font-bold">4+</h2>
+              <p className="mt-1">Satisfied clients</p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div className="mr-5 text-gray-300">
+              <HiOutlineChartBar size={50} />
+            </div>
+            <div>
+              <h2 className="text-3xl font-bold">3+</h2>
+              <p className="mt-1">Years of experience</p>
+            </div>
+          </div>
         </div>
       </section>
     </>
